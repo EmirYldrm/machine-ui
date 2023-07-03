10f1cf1a3cdb2cbad50b0e7a90ef58ee59782f71
emir yildirim <yildirimemir149@gmail.com>
2023-07-03 20:39:10 +0300

    Motor sınıfı oluşuturuldu. Motor sınıfları instanciate edilecek.

---
+++ KontrolPaneli/src/Model/Motor.java
@@ -0,0 +1,16 @@
+package Model;
+
+import Model.MachineConfig;
+public class Motor {
+
+	public float kasnakOran;
+	public long oneMMStepcount;
+	
+	public Motor() {}
+	
+	public long grToStep(float hacim) {
+	
+		return 0;
+	}
+	
+}
