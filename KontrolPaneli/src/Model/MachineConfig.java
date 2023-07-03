60c4e218fde20c54b41d730a4036eb3498a1178a
emir yildirim <yildirimemir149@gmail.com>
2023-07-03 20:38:23 +0300

    Makinin sabit özelliklerinin bulunacaği class oluşuturuldu.

---
+++ KontrolPaneli/src/Model/MachineConfig.java
@@ -0,0 +1,15 @@
+package Model;
+
+
+
+public class MachineConfig {
+	
+	public static final int kalipMaxMM = 135;
+	public static final int enjksiyonMaxMM = 145;
+	public static final int milHatve = 5;
+	public static final float enjeksiyonKasnakOrani = 3.2f;
+	public static final float helezonKasnakOran = 4.8f;
+	public static final float kalipKasnakOran = 10.24f;
+	public static final float StepPerMM = 409.6f;
+	
+}
