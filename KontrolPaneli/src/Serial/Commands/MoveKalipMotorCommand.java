4c0c1794cf722d30d85aeb2b50421073c16197fe
emir yildirim <yildirimemir149@gmail.com>
2023-07-07 20:24:39 +0300

    kalip motorunu kontrol edecek komut için sınıf oluşturuldu

---
+++ KontrolPaneli/src/Serial/Commands/MoveKalipMotorCommand.java
@@ -0,0 +1,23 @@
+package Serial.Commands;
+
+import Model.MachineInfo;
+
+public class MoveKalipMotorCommand implements ICommand {
+
+	
+	private MachineInfo machineInfo;
+	
+	
+	@Override
+	public void setParameter(String parameter) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void execute() {
+		
+
+	}
+
+}
