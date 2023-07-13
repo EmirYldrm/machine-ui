261e72ef8fb2e8db3ad3d8b897ad0045d50d61c3
emir yildirim <yildirimemir149@gmail.com>
2023-07-13 15:13:04 +0300

    Eksik olan komutlar eklendi ve classlar oluşturuldu

---
+++ KontrolPaneli/src/Serial/Commands/SetProcessPinLengthCommand.java
@@ -0,0 +1,17 @@
+package Serial.Commands;
+
+public class SetProcessPinLengthCommand implements ICommand{
+
+	@Override
+	public void setParameter(String parameter) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void execute() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
