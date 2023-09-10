282a08c698f1a713850fa4dffc917cc2d012dcbf
emir yildirim <yildirimemir149@gmail.com>
2023-09-10 18:47:51 +0300

    video için gerekli paket eklendi

--- KontrolPaneli/src/module-info.java
+++ KontrolPaneli/src/module-info.java
@@ -4,8 +4,9 @@ module KontrolPaneli {
 	requires com.fazecast.jSerialComm;
 	requires com.jfoenix;
 	requires javafx.graphics;
+	requires javafx.media;
 	
-	opens application to javafx.graphics, javafx.fxml;
-	opens Controllers to javafx.graphics, javafx.fxml;
+	opens application to javafx.graphics, javafx.fxml, javafx.scene.media;
+	opens Controllers to javafx.graphics, javafx.fxml, javafx.scene.media;
 	
 }
