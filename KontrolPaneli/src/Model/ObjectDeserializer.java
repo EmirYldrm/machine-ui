0471740b0152959b2e80d883d06215791019c52c
emir yildirim <yildirimemir149@gmail.com>
2023-09-13 18:28:16 +0300

    Serialize ve deserialize işlemi fonksiyonle olarak enteger edildi

--- KontrolPaneli/src/Model/ObjectDeserializer.java
+++ KontrolPaneli/src/Model/ObjectDeserializer.java
@@ -1,18 +1,29 @@
 package Model;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.Serializable;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 
 public class ObjectDeserializer {
     public Serializable deserializeObject(String fileName) throws IOException, ClassNotFoundException {
-    			FileInputStream fileIn = new FileInputStream(fileName);
-                ObjectInputStream in = new ObjectInputStream(fileIn);
+    	
+    	
+    	try (InputStream inputStream =new FileInputStream(fileName)) {
+          
+                ObjectInputStream in = new ObjectInputStream(inputStream);
                 Serializable ser = (Serializable) in.readObject();
                 in.close();
-                fileIn.close();
                 return ser;
+        
+        }catch(IOException e){
+        	throw e;
+        }
         
         
     }
--- KontrolPaneli/src/Model/ObjectSerializer.java
+++ KontrolPaneli/src/Model/ObjectSerializer.java
@@ -5,8 +5,10 @@ import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
+
 public class ObjectSerializer {
 	public void serializeObject(String fileName, Serializable object) throws IOException {
+		
         try (FileOutputStream fileOut = new FileOutputStream(fileName);
 			ObjectOutputStream out = new ObjectOutputStream(fileOut)) {
 			out.writeObject(object);
